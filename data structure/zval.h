cb4de3215cd7c375e7f405359670d3f9356b7d75
COFFEE <353238473@qq.com>
2018-05-25 09:55:02 +0800

    zval结构定义及所在目录

---
+++ data structure/zval.h
@@ -0,0 +1,31 @@
+/***
+ * /Zend/zend_types.h
+ */
+ 
+struct _zval_struct {
+	zend_value        value;			/* value */
+	union {
+		struct {
+			ZEND_ENDIAN_LOHI_3(
+				zend_uchar    type,			/* active type */
+				zend_uchar    type_flags,
+				union {
+					uint16_t  call_info;    /* call info for EX(This) */
+					uint16_t  extra;        /* not further specified */
+				} u)
+		} v;
+		uint32_t type_info;
+	} u1;
+	union {
+		uint32_t     next;                 /* hash collision chain */
+		uint32_t     cache_slot;           /* cache slot (for RECV_INIT) */
+		uint32_t     opline_num;           /* opline number (for FAST_CALL) */
+		uint32_t     lineno;               /* line number (for ast nodes) */
+		uint32_t     num_args;             /* arguments number for EX(This) */
+		uint32_t     fe_pos;               /* foreach position */
+		uint32_t     fe_iter_idx;          /* foreach iterator index */
+		uint32_t     access_flags;         /* class constant access flags */
+		uint32_t     property_guard;       /* single property guard */
+		uint32_t     extra;                /* not further specified */
+	} u2;
+};
